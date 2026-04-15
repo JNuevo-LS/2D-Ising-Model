5dc84341524502fb00e42c40dff60c10aeeee920
JNuevo-LS <JNLiSn@proton.me>
2026-04-15 13:51:29 -0400

    opted for random selection, added heat bath algorithm

--- mc.py
+++ mc.py
@@ -1,10 +1,9 @@
-import random
 import numpy as np
 import numba
 
 # CONSTANTS
 J = 1.0
-h = 0.0
+h = 0.0 # Magnetic Field
 k_B = 1
 T = 1.5
 beta = 1 / (k_B * T)
@@ -18,62 +17,99 @@ def initial_state(N):
 def energy_per_spin(state):
     N = state.shape[0]
     E = 0.0
+    # Sum over every pair
     for i in range(N):
         for j in range(N):
             E += -J * state[i, j] * (state[(i + 1) % N, j] + state[i, (j + 1) % N])
             E += -h * state[i, j]
     return E / (N * N)
 
+@numba.jit
+def sum_over_spins(state):
+    N = state.shape[0]
+    s = 0
+    for i in range(N):
+        for j in range(N):
+            s += state[i, j]
+    return s/(N*N)
 
 @numba.jit
-def step(state, beta):
-    def delta_E(state, i, j) -> float:
-        N = len(state)
-        s = state[i][j]
+def nearest_neighbors_sum(state, i, j):
+    N = len(state)
+    return (
+        state[(i + 1) % N][j]
+        + state[i][(j + 1) % N]
+        + state[(i - 1) % N][j]
+        + state[i][(j - 1) % N]
+    )
 
-        nearest_neighbors_sum = (
-            state[(i + 1) % N][j]
-            + state[i][(j + 1) % N]
-            + state[(i - 1) % N][j]
-            + state[i][(j - 1) % N]
-        )
-        return 2.0 * s * (J * nearest_neighbors_sum + h)
+@numba.jit
+def step_heat_bath(state, beta):
+    for _ in range(len(state)**2):
+        i, j = np.random.choice(len(state)), np.random.choice(len(state))
+        nn = nearest_neighbors_sum(state, i, j)
 
-    for i in range(len(state)):
-        for j in range(len(state[0])):
-            dE: float = delta_E(state, i, j)
+        p1, p2 = np.exp(beta * nn), np.exp(-beta * nn)
+        Z = p1+p2
+        p = p1/Z
 
-            # Standard Metropolis accept/reject
-            if dE <= 0 or random.random() < np.exp(-beta * dE):
-                state[i, j] *= -1
+        if np.random.random() < p:
+            state[i, j] = 1
+        else:
+            state[i, j] = -1
+
+@numba.jit
+def step_metropolis(state, beta):
+    for _ in range(len(state)**2):
+        i, j = np.random.choice(len(state)), np.random.choice(len(state))
+        nn = nearest_neighbors_sum(state, i, j)
+        dE = 2.0 * state[i, j] * (J * nn + h)
+
+        # Standard Metropolis accept/reject
+        if dE <= 0 or np.random.random() < np.exp(-beta * dE):
+            state[i, j] *= -1
 
 
-def simulate(state, beta, epochs):
+def simulate_metropolis(state, beta, epochs):
     history = np.empty((epochs + 1, *state.shape), dtype=np.int8)
     energies = np.empty(epochs)
     history[0] = state
 
     for i in range(epochs):
-        step(state, beta)
+        step_metropolis(state, beta)
         history[i + 1] = state
         energies[i] = energy_per_spin(state)
     return history, energies
 
+def simulate_heat_bath(state, beta, epochs):
+    history = np.empty((epochs + 1, *state.shape), dtype=np.int8)
+    energies = np.empty(epochs)
+    history[0] = state
+
+    for i in range(epochs):
+        step_heat_bath(state, beta)
+        history[i + 1] = state
+        energies[i] = energy_per_spin(state)
+    return history, energies
 
 def fit_exponential(initial_value, final_value, last_step):
     return (1 / last_step) * np.log(final_value / initial_value)
 
 
-def simulate_changing_T(state, initial_beta, final_beta, epochs):
+def simulate_changing_T_heat_bath(state, initial_beta: float, final_beta: float, epochs: int, linear: bool):
     history = np.empty((epochs + 1, *state.shape), dtype=np.int8)
     energies = np.empty(epochs)
     history[0] = state
 
-    exponential_rate = fit_exponential(initial_beta, final_beta, epochs)
+    if linear:
+        slope = (final_beta - initial_beta) / epochs
+        beta = lambda i: initial_beta + (i * slope)
+    else:
+        exponential_rate = fit_exponential(initial_beta, final_beta, epochs)
+        beta = lambda i: initial_beta * np.exp(exponential_rate * i)
 
     for i in range(epochs):
-        beta = initial_beta * np.exp(exponential_rate * i)
-        step(state, beta)
+        step_heat_bath(state, beta(i))
         history[i + 1] = state
         energies[i] = energy_per_spin(state)
 
